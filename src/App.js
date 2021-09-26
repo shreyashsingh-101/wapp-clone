84683e82f6d9c67ce4fad5bab9e0dae9a17d0117
Shreyash Singh <Rohitsingh9450@hotmail.com>
2021-09-26 16:26:10 +0530

    completed initial build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import Pusher from 'pusher-js';
 import "./App.css";
 import Sidebar from "./sidebar";
-import Chat from "./chat";
+import Chat from "./Chat";
+import axios from 'axios';
+
+
 function App() {
+
+  const [messages, setMessages] = useState([]);;
+
+  useEffect(() => {
+    axios.get('http://localhost:9000/messages/sync')
+      .then(response =>{
+        setMessages(response.data);
+      })
+
+  },[])
+
+  useEffect(() => {
+    var pusher = new Pusher('baaa43a26f3b8c202ff6', {
+      cluster: 'eu'
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      setMessages([...messages, data]);
+    });
+
+     return () => {
+        channel.unbind_all();
+        channel.unsubscribe();
+      }
+
+  }, [messages])
+
+  console.log(messages);
+
+
   return (
     <div className="app">
       
@@ -9,7 +45,7 @@ function App() {
         {/* Sidebar */}
         <Sidebar />
         {/* Chat */}
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
--- src/chat.js
+++ src/chat.js
@@ -1,15 +1,74 @@
-import React from 'react'
-import "./chat.css"
-import { Avatar } from '@material-ui/core'
+import { React, useState } from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+import axios from 'axios';
 
-const chat = () => {
-    return (
-        <div className="chat">
-            <div className="chat__header">
-                <Avatar />
-            </div>
+const Chat = ({ messages }) => {
+
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await axios.post('http://localhost:9000/messages/new',{
+      message : input,
+      name: "Shreyash",
+      timestamp: "Just Now",
+      received: false
+    });
+
+    setInput('');
+  }  
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
         </div>
-    )
-}
 
-export default chat
+        <div className="class__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        {messages.map((message) => (
+          <p className={`chat__message ${!message.received && "chat__sender"}`}>
+            <span className="chat__name">{messages.name}</span>
+            {message.message}
+            <span className="chat__timestamp">{message.timestamp}</span>
+          </p>
+        ))}
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+
+        <form>
+          <input value={input} onChange={(e) => {setInput(e.target.value)}} placaholder="Type a message" type="text" name="" id="" />
+          <button onClick={ sendMessage } type="submit">Send a message</button>
+          <div className="mic__icon">
+            <MicIcon />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
